e2953ee0410243f4fb1422997b74469041900060
Reuben John <reubezzz1@gmail.com>
2013-07-09 21:22:09 +0300

    Gravity

--- scripts/scene1.cpp
+++ scripts/scene1.cpp
@@ -1,19 +1,19 @@
 #include "headers/physim.h"
 #include <iostream>
 using namespace std;
+SDL_Event event;
 int main(int argc,char* args[])
 {
+	int frame=0;
 	PHYSIM psm((SDL_Rect){720,50});
 	SDL_Surface* scr=SDL_SetVideoMode(720,480,32,SDL_SWSURFACE);
-	SDL_FillRect(scr,&scr->clip_rect,SDL_MapRGB(scr->format,0,0xFF,0xFF));
-
-	vector pos={10,10};
-	vector dim=pos;
-	particle particle1(pos,dim,"images/dot.png");
-	while(1)
+	particle* particle1;
+	particle1=new particle("images/dot.png");
+			SDL_FillRect(scr,&scr->clip_rect,SDL_MapRGB(scr->format,0,0xFF,0xFF));
+	while(!psm.ended)
 	{
 		//=================================initialisation
-
+		SDL_FillRect(scr,&scr->clip_rect,SDL_MapRGB(scr->format,0,0xFF,0xFF));
 		//=================================
 
 		//_________________________________
@@ -25,13 +25,26 @@ int main(int argc,char* args[])
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 		//.................................graphic rendering
-		particle1.display(scr);
+		particle1->display(scr);
 		//.................................
 
 		//---------------------------------termination
 		SDL_Flip(scr);
+		SDL_Delay(50);
+		frame++;
+		if(frame>1000)
+			psm.ended=true;
+
+		while( SDL_PollEvent( &event ) )
+		{
+			if( event.type == SDL_QUIT )
+				{
+		                psm.ended=true;
+		        }
+		}
+
 		//---------------------------------
 	}
-	SDL_Delay(1000);
+	delete particle1;
 	return 0;
 }
