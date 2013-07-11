0a7c0b9ad7f0dda9727eaa71e37739a7914bc7ac
Reuben John <reubezzz1@gmail.com>
2013-07-11 18:14:35 +0300

    fixed major integrate() and collision() bugs

--- scripts/scene1.cpp
+++ scripts/scene1.cpp
@@ -2,23 +2,25 @@
 #include <iostream>
 using namespace std;
 SDL_Event event;
+int N=1;
 int main(int argc,char* args[])
 {
 	PHYSIM scene1((SDL_Rect){720,50});
 	SDL_Surface* scr=SDL_SetVideoMode(720,480,32,SDL_SWSURFACE);
 
 	SDL_Surface* dot=loadimage("images/dot.png");
-	particle** particles = new  particle*[10];
-	for(int i=0;i<10;i++)
+	particle** particles = new  particle*[1000];
+	for(int i=0;i<1000;i++)
 	{
-		particles[i]=new particle((vect){10,10,0},(vect){20,20,0},dot);
-		particles[i]->addacc((vect){0,98,0});
+		particles[i]=new particle(dot);
+		particles[i]->addacc((vect){0,980,0});
 	}
 
 	while(!scene1.ended)
 	{
 		//=================================initialisation
 		scene1.initiateframe();
+		N+=0;
 		//=================================
 
 		//_________________________________
@@ -27,17 +29,21 @@ int main(int argc,char* args[])
 
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~physics simulation
 		ofstream fout("framelog.txt",ios::app);
-		fout<<scene1.runtime.elapse()<<"		"<<scene1.frametimer.currentfps()<<"		"<<scene1.frametimer.deltatime()<<'\n';
-		fout.close();
-		for(int i=0;i<10;i++)
+		fout<<scene1.runtime.elapse()<<"		"<<scene1.frametimer.currentfps()<<"		"<<scene1.frametimer.deltatime()<<"	"<<particles[0]->vel.x<<"	"<<particles[0]->pos.x<<'\n';
+		for(int i=0;i<N;i++)
 		{
-			if(!particles[i]->globalcollision(scene1.frametimer.deltatime()))
-				particles[i]->integrate(scene1.frametimer.deltatime());
+				if(!particles[i]->globalcollision(scene1.frametimer.deltatime()))
+				{
+					particles[i]->integrate(scene1.frametimer.deltatime());
+				}
+				else
+					fout<<"Nooooo!!!!!!";
 		}
+		fout.close();
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 		//.................................graphic rendering
-		for(int i=0;i<10;i++)
+		for(int i=0;i<N;i++)
 			particles[i]->display(scr);
 		//.................................
 
@@ -48,6 +54,8 @@ int main(int argc,char* args[])
 				{
 		                scene1.ended=true;
 		        }
+			for(int i=0;i<N;i++)
+				particles[i]->trial(event);
 		}
 		scene1.terminateframe((SDL_Color){0,0xFF,0});
 		if(scene1.frametimer.currentframe()>10000)
