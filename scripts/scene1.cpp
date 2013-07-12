3e9a0b419255b988b5f5727c44af37963ae91fd1
Reuben John <reubezzz1@gmail.com>
2013-07-13 01:08:57 +0300

    unstable vector dynamic management

--- scripts/scene1.cpp
+++ scripts/scene1.cpp
@@ -6,21 +6,24 @@ int N=1;
 int main(int argc,char* args[])
 {
 	PHYSIM scene1((SDL_Rect){720,50});
-	SDL_Surface* scr=SDL_SetVideoMode(720,480,32,SDL_SWSURFACE);
-
+	SDL_FillRect(scr,&scr->clip_rect,SDL_MapRGB(scr->format,0xFF,0xFF,0xFF));
+	SDL_Flip(scr);
 	SDL_Surface* dot=loadimage("images/dot.png");
-	PARTICLE** particles = new  PARTICLE*[1000];
-	for(int i=0;i<1000;i++)
+	PARTICLE temp(dot);
+	scene1.particle.push_back(temp);
+	scene1.particle.push_back(temp);
+	scene1.particle.at(0).display(scr);
+	SDL_Flip(scr);
+	SDL_Delay(100);
+	for(unsigned int i=0;i<1;i++)
 	{
-		particles[i]=new PARTICLE(dot);
-		particles[i]->addacc((vect){1,9.8,0});
+		scene1.particle.at(i).addacc((vect){0.011,0.098,0});
 	}
 
 	while(!scene1.ended)
 	{
 		//=================================initialisation
 		scene1.initiateframe();
-		N+=1;
 		//=================================
 
 		//_________________________________
@@ -30,19 +33,20 @@ int main(int argc,char* args[])
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~physics simulation
 		ofstream fout("framelog.txt",ios::app);
 		fout<<scene1.runtime.elapse()<<"		"<<scene1.frametimer.currentfps()<<"		"<<scene1.frametimer.deltatime()<<"\n";
-		for(int i=0;i<N;i++)
-		{
-				if(!particles[i]->globalcollision(scene1.frametimer.deltatime()))
-				{
-					particles[i]->integrate(scene1.frametimer.deltatime());
-				}
-		}
+		if(scene1.frametimer.currentframe()>10)
+			for(unsigned int i=0;i<1;i++)
+			{
+					if(!scene1.particle.at(i).globalcollision(scene1.frametimer.deltatime()))
+					{
+						scene1.particle.at(i).integrate(scene1.frametimer.deltatime());
+					}
+			}
 		fout.close();
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 		//.................................graphic rendering
-		for(int i=0;i<N;i++)
-			particles[i]->display(scr);
+		for(unsigned int i=0;i<1;i++)
+			scene1.particle.at(i).display(scr);
 		//.................................
 
 		//---------------------------------termination
@@ -52,14 +56,11 @@ int main(int argc,char* args[])
 				{
 		                scene1.ended=true;
 		        }
-			for(int i=0;i<N;i++)
-				particles[i]->trial(event);
 		}
 		scene1.terminateframe((SDL_Color){0,0xFF,0});
 		if(scene1.frametimer.currentframe()>10000)
 			scene1.ended=true;
 		//---------------------------------
 	}
-	delete [] particles;
 	return 0;
 }
