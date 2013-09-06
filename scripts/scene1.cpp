cbe5da0afc22169ea15851f3e8be4444a152eaa0
Reuben John <reubezzz1@gmail.com>
2013-09-06 16:08:56 +0300

    perfected apparent positioning of particles with respect to camera position

--- scripts/scene1.cpp
+++ scripts/scene1.cpp
@@ -5,24 +5,24 @@ SDL_Event event;
 int main(int argc,char* args[])
 {
 	ofstream fout("framelog.txt");
-	PHYSIM scene1((vect){998,755,50});
+	PHYSIM scene1((vect){998,755,2000});
 	SDL_FillRect(scr,&scr->clip_rect,SDL_MapRGB(scr->format,0xDD,0xDD,0xDD));
 	SDL_Flip(scr);
 	SDL_Surface* skyline=loadimage("images/White Cube.jpg");
 	SDL_Surface* dot=loadimage("images/dot.png");
-	//scene1.genparticle(dot)->addvel(random((vect){-50,-50,0},(vect){50,50,0}));
+	//scene1.gensphere(dot)->addvel(random((vect){-50,-50,0},(vect){50,50,0}));
 	//SDL_Delay(500);
 
 	while(!scene1.ended)
 	{
 		if(scene1.frametimer.currentframe()%50==0)
 		{
-			scene1.genparticle(loadimage("images/dot.png"))->addvel(random((vect){-10,-10,-5},(vect){10,10,5}));
+			scene1.gensphere(loadimage("images/dot.png"))->addvel(random((vect){-10,-10,0},(vect){10,10,0}));
 		}
 		//=================================initialisation
 		scene1.initiateframe();
-		for(unsigned int i=1;i<scene1.particle.size();i++)
-			scene1.particle[i]->newframe();
+		for(unsigned int i=1;i<scene1.sphere.size();i++)
+			scene1.sphere[i]->newframe();
 		//=================================
 
 		//_________________________________user interaction
@@ -31,24 +31,26 @@ int main(int argc,char* args[])
 
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~physics simulation
 		if(scene1.frametimer.currentframe()>10)
-			for(unsigned int i=1;i<scene1.particle.size()-1;i++)
+			for(unsigned int i=1;i<scene1.sphere.size()-1;i++)
 			{
-				scene1.particle[i]->addforce((vect){0,98,0});
-					for(unsigned int j=i+1;j<scene1.particle.size();j++)
+				scene1.sphere[i]->addforce((vect){0,98,0});
+					for(unsigned int j=i+1;j<scene1.sphere.size();j++)
 					{
-							scene1.particle[i]->collision(*scene1.particle[j],scene1.frametimer.deltatime());
+							scene1.sphere[i]->collision(*scene1.sphere[j],scene1.frametimer.deltatime());
 					}
-					if(!scene1.particle[i]->globalcollision((void*)&scene1,scene1.frametimer.deltatime()))
+					if(!scene1.sphere[i]->globalcollision((void*)&scene1,scene1.frametimer.deltatime()))
 					{
-						scene1.particle[i]->integrate(scene1.frametimer.deltatime());
+						scene1.sphere[i]->integrate(scene1.frametimer.deltatime());
 					}
 			}
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 		//.................................graphic rendering
-		for(unsigned int i=1;i<scene1.particle.size()-1;i++)
-			if(!scene1.particle[i]->justcollided())
-				scene1.particle[i]->display((void*)&scene1);
+		for(unsigned int i=1;i<scene1.sphere.size()-1;i++)
+		{
+			if(!scene1.sphere[i]->justcollided())
+				scene1.sphere[i]->display((void*)&scene1);
+		}
 		//.................................
 
 		//---------------------------------termination
