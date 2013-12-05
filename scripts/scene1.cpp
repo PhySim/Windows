7d26bd050b9cdba3f846b2b16f2b31c934b1a36d
Reuben John <reubezzz1@gmail.com>
2013-12-05 19:41:29 +0300

    CELL class created sucessfully + find_food() member created and tested to accelerate cell to food

--- scripts/scene1.cpp
+++ scripts/scene1.cpp
@@ -2,7 +2,6 @@
 #include <string.h>
 using namespace std;
 SDL_Event event;
-int cell=1234;
 
 int main(int argc,char* args[])
 {
@@ -26,34 +25,36 @@ int main(int argc,char* args[])
 	fps_label.set_position(scrdim.x-80,1);
 	graphicstring fps("0",14,kerater,500);
 	fps.set_position(scrdim.x-50,1);
-	graphicstring particle_n_label("Sphere number:",14,kerater,100);
+	graphicstring particle_n_label("Particle number:",14,kerater,100);
 	particle_n_label.set_position(scrdim.x-135,25);
 	graphicstring particle_n("0",14,kerater,100);
 	particle_n.set_position(scrdim.x-25,25);
+	graphicstring bugs("no bugs",14,kerater,500);
+	bugs.set_position(scrpos.x+25,scrdim.y-25);
 
 	//loading various required images, and sounds
 	SDL_Surface* skyline=loadimage(file_loc(buf,image_loc,"White Cube.jpg"));
-	SDL_Surface* sphere=loadimage(file_loc(buf,image_loc,"blue ball.png"));
+	SDL_Surface* blue_sphere=loadimage(file_loc(buf,image_loc,"blue ball.png"));
 	//Mix_Chunk *bounce_loud=Mix_LoadWAV("audio/Bounce Loud.wav");
 	Mix_Chunk *bounce=Mix_LoadWAV(file_loc(buf,audio_loc,"Bounce.wav"));
 	Mix_Chunk *bounce_loud=Mix_LoadWAV(file_loc(buf,audio_loc,"Bounce Loud.wav"));
-	Mix_Chunk *roll=Mix_LoadWAV(file_loc(buf,audio_loc,"roll.wav"));
 	Mix_Chunk *mash=Mix_LoadWAV(file_loc(buf,audio_loc,"mash.wav"));
 
 	//main loop within which most of the processing starts
 	while(!scene1.ended)	//variable that controls the end of the program
 	{
-		//generates a new blue ball object every 100 frames at a random position
+		//generates a new blue ball object every ... frames at a random position
 		if(scene1.frametimer.currentframe()%100==0)
 		{
-			SPHERE* TEMP=scene1.gensphere(loadimage(blue_ball),cell,randomposition,(vect){20,20,20},1);
+			CELL* TEMP=new CELL((void*)&scene1,loadimage(blue_ball),randomposition,(vect){20,20,20},1);
+			scene1.cells.push_back(TEMP);
 			if(TEMP)
 				TEMP->addvel(random((vect){-10,-10,0},(vect){10,10,50}));
 		}
 		//=================================initialisation
 		scene1.initiateframe();
-		for(unsigned int i=1;i<scene1.sphere.size();i++)	//preparing all objects currently on screen for a new frame
-			scene1.sphere[i]->newframe();
+		for(unsigned int i=0;i<scene1.cells.size();i++)	//preparing all objects currently on screen for a new frame
+			scene1.cells[i]->newframe();
 		//=================================
 
 		//_________________________________user interaction
@@ -74,7 +75,8 @@ int main(int argc,char* args[])
 						newpos.z=0;
 					else if(newpos.z>scene1.scrdim.z)
 						newpos.z=scrdim.z;
-					scene1.gensphere(loadimage(blue_ball),cell,newpos,(vect){20,20,20});
+					scene1.cells.push_back(new CELL((void*)&scene1,loadimage(blue_ball),newpos,(vect){20,20,20}));
+					//scene1.gensphere(loadimage(blue_ball),newpos,(vect){20,20,20});
 			    }
 		}
 		//_________________________________
@@ -83,53 +85,53 @@ int main(int argc,char* args[])
 		if(scene1.frametimer.currentframe()>10)
 		{
 			scene1.MoveCamera();
-			for(unsigned int i=1;i<scene1.sphere.size();i++)
+			for(unsigned int i=0;i<scene1.cells.size();i++)
 			{
-				scene1.sphere[i]->addacc((vect){0,98,0});
-					for(unsigned int j=1;j<scene1.sphere.size();j++)
+				if(scene1.frametimer.currentframe()>50)
+				{
+					CELL* food=scene1.cells[i]->find_food((void*)&scene1);
+					if(food)
+					{
+						vect a=(food->position()-scene1.cells[i]->position()).dir()*20;
+						bugs.set((double)a.mag());
+						scene1.cells[i]->addacc(a);
+					}
+				}
+
+					for(unsigned int j=0;j<scene1.cells.size();j++)
 					{
 						if(i!=j)
 						{
-							if(scene1.sphere[i]->collision(*scene1.sphere[j])&&scene1.sphere[i]->continuous_contact()<2)
+							if(scene1.cells[i]->collision(*scene1.cells[j])&&scene1.cells[i]->continuous_contact()<2)
 								Mix_PlayChannel( -1, bounce_loud, 0 );
-							if(scene1.sphere[i]->spring_connected(scene1.sphere[j]))
-								scene1.sphere[i]->spring(*scene1.sphere[j]);
-							else if(scene1.sphere[i]->position().separation(scene1.sphere[j]->position())<60)
-								scene1.sphere[i]->connect_spring(scene1.sphere[j],60,1);
 						}
 					}
-					if(scene1.sphere[i]->time_since_last_collision()>100)
+					if(scene1.cells[i]->time_since_last_collision()>100)
 					{
-						if(scene1.sphere[i]->globalcollision((void*)&scene1,scene1.frametimer.deltatime()))
+						if(scene1.cells[i]->globalcollision((void*)&scene1,scene1.frametimer.deltatime()))
 						{
 							Mix_PlayChannel( -1, bounce, 0 );
 						}
 					}
-					else if(scene1.sphere[i]->time_since_last_collision()<50)
-					{
-						if(scene1.sphere[i]->globalcollision((void*)&scene1,scene1.frametimer.deltatime())&&scene1.sphere[i]->velocity().mag()>1)
-						{
-							Mix_PlayChannel( -1, roll, 0 );
-						}
-					}
-					scene1.sphere[i]->integrate(scene1.frametimer.deltatime());
+					scene1.cells[i]->integrate(scene1.frametimer.deltatime());
 			}
 		}
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 		//.................................graphic rendering
-		scene1.DisplaySortSpheres();	//makes sure objects closer to the camera are displayed on top of objects further away
-		for(unsigned int i=1;i<scene1.sphere.size();i++)	//diaplay all objects on screen
+		scene1.DisplaySortCells();	//makes sure objects closer to the camera are displayed on top of objects further away
+		for(unsigned int i=0;i<scene1.cells.size();i++)	//display all objects on screen
 		{
-			scene1.sphere[i]->display((void*)&scene1);
+			scene1.cells[i]->display((void*)&scene1);
 		}
 		//display various texts on screen
 		message.display();
 		fps_label.display();
 		fps.display();
 		particle_n_label.display();
-		particle_n.set((int)scene1.sphere.size());	//store the number of particles currently on screen into the particle_n graphicstring object
+		particle_n.set(int(scene1.spheres.size()+scene1.cells.size()));	//store the number of particles currently on screen into the particle_n graphicstring object
 		particle_n.display();
+		bugs.display();
 		//.................................
 
 		//---------------------------------termination
@@ -142,9 +144,8 @@ int main(int argc,char* args[])
 	//deallocate various dynamic allocations
 	Mix_FreeChunk(bounce);
 	Mix_FreeChunk(mash);
-	Mix_FreeChunk(roll);
-	if(sphere)
-		SDL_FreeSurface(sphere);
+	if(blue_sphere)
+		SDL_FreeSurface(blue_sphere);
 	if(skyline)
 		SDL_FreeSurface(skyline);
 	return 0;
