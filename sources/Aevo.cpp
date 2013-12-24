faced593506ada8a4776d096109c27ba85f69180
Reuben John <reubezzz1@gmail.com>
2013-12-24 03:54:19 +0300

    made intergravity.cpp ...awsome...

--- sources/Aevo.cpp
+++ sources/Aevo.cpp
@@ -106,33 +106,32 @@ int main(int argc,char* args[])
 	file_loc(kerater,font_loc,"KeraterMedium.ttf");
 	file_loc(cell_loc,image_loc,"cell.png");
 
-	PHYSIM scene1((vect){998,755,400});	//creating the main object
-	SDL_FillRect(scene1.scr,&scene1.scr->clip_rect,SDL_MapRGB(scene1.scr->format,0xDD,0xDD,0xDD));	//intitiate the entire screen with grey color
-	graphicstring message(&scene1,"scene1",28,kerater,10000);	//initiate the name of the scene
+	PHYSIM aevo((vect){998,755,400});	//creating the main object
+	graphicstring message(&aevo,"aevo",28,kerater,10000);	//initiate the name of the scene
 	message.set_position(1,1);							//set its position
-	message.display(scene1);									//display it on screen at that position
-	graphicstring bugs(&scene1,"no bugs",14,kerater,500);
+	message.display(aevo);									//display it on screen at that position
+	graphicstring bugs(&aevo,"no bugs",14,kerater,500);
 	bugs.set_position(scrpos.x+25,scrdim.y-25);
 	//loading various required images, and sounds
 	bool fail=load_media();
 	if(fail)
 	{
 		bugs.set("failed to load media files");
-		bugs.display(scene1);
-		SDL_Flip(scene1.scr);										//update the screen to allow the user to see the latest version of it
-		scene1.ended=true;
+		bugs.display(aevo);
+		SDL_Flip(aevo.scr);										//update the screen to allow the user to see the latest version of it
+		aevo.ended=true;
 		SDL_Delay(2500);
 	}
-	SDL_Flip(scene1.scr);										//update the screen to allow the user to see the latest version of it
+	SDL_Flip(aevo.scr);										//update the screen to allow the user to see the latest version of it
 
 	//initiating various other texts to be labelled in screen
-	graphicstring fps_label(&scene1,"FPS:",14,kerater,10000);
+	graphicstring fps_label(&aevo,"FPS:",14,kerater,10000);
 	fps_label.set_position(scrdim.x-80,1);
-	graphicstring fps(&scene1,"0",14,kerater,500);
+	graphicstring fps(&aevo,"0",14,kerater,500);
 	fps.set_position(scrdim.x-50,1);
-	graphicstring particle_n_label(&scene1,"Particle number:",14,kerater,100);
+	graphicstring particle_n_label(&aevo,"Particle number:",14,kerater,100);
 	particle_n_label.set_position(scrdim.x-135,25);
-	graphicstring particle_n(&scene1,"0",14,kerater,100);
+	graphicstring particle_n(&aevo,"0",14,kerater,100);
 	particle_n.set_position(scrdim.x-25,25);
 	bugs.set_position(scrpos.x+25,scrdim.y-25);
 
@@ -150,20 +149,20 @@ int main(int argc,char* args[])
 	load_first_cell();
 
 	//main loop within which most of the processing starts
-	while(!scene1.ended)	//variable that controls the end of the program
+	while(!aevo.ended)	//variable that controls the end of the program
 	{
 		//generates a new blue ball object every ... frames at a random position
-		if(scene1.frametimer.currentframe()%1000==0)
+		if(aevo.frametimer.currentframe()%1000==0)
 		{
-			CELL* TEMP=new CELL(scene1,loadimage(cell_loc),randomposition,first_DNA);
-			scene1.cells.push_back(TEMP);
+			CELL* TEMP=new CELL(aevo,loadimage(cell_loc),randomposition,first_DNA);
+			aevo.cells.push_back(TEMP);
 			if(TEMP)
 				TEMP->addvel(random((vect){-10,-10,0},(vect){10,10,50}));
 		}
 		//=================================initialisation
-		scene1.initiateframe();
-		for(unsigned int i=0;i<scene1.cells.size();i++)	//preparing all objects currently on screen for a new frame
-			scene1.cells[i]->newframe();
+		aevo.initiateframe();
+		for(unsigned int i=0;i<aevo.cells.size();i++)	//preparing all objects currently on screen for a new frame
+			aevo.cells[i]->newframe();
 		//=================================
 
 		//_________________________________user interaction
@@ -171,87 +170,87 @@ int main(int argc,char* args[])
 		{
 			if( event.type == SDL_QUIT )	//check if the close button has been pressed
 			{
-					scene1.ended=true;
+					aevo.ended=true;
 			}
-			scene1.mousemotion(event);		//handle mouse motion
-			scene1.HandleCameraMovement(event);	//handle user input related to camera motion
+			aevo.mousemotion(event);		//handle mouse motion
+			aevo.HandleCameraMovement(event);	//handle user input related to camera motion
 			if( event.type == SDL_MOUSEBUTTONDOWN )	//check if the left mouse button has been pressed and then generates a new object at that position
 				if( event.button.button == SDL_BUTTON_LEFT )
 			    {
-					vect newpos=scene1.mousepos;
-					newpos.z=scene1.cameraPosition().z+300;
+					vect newpos=aevo.mousepos;
+					newpos.z=aevo.cameraPosition().z+300;
 					if(newpos.z<0)
 						newpos.z=0;
-					else if(newpos.z>scene1.scrdim.z)
+					else if(newpos.z>aevo.scrdim.z)
 						newpos.z=scrdim.z;
-					scene1.cells.push_back(new CELL(scene1,copy_surface(cell),newpos,first_DNA));
-					//scene1.gensphere(loadimage(blue_ball),newpos,(vect){20,20,20});
+					aevo.cells.push_back(new CELL(aevo,copy_surface(cell),newpos,first_DNA));
+					//aevo.gensphere(loadimage(blue_ball),newpos,(vect){20,20,20});
 			    }
 		}
 		//_________________________________
 
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~physics simulation
-		if(scene1.frametimer.currentframe()>10)
+		if(aevo.frametimer.currentframe()>10)
 		{
-			scene1.MoveCamera();
-			for(unsigned int i=0;i<scene1.cells.size();i++)
+			aevo.MoveCamera();
+			for(unsigned int i=0;i<aevo.cells.size();i++)
 			{
-				if(scene1.frametimer.currentframe()>100)
+				if(aevo.frametimer.currentframe()>100)
 				{
-					CELL* food=scene1.cells[i]->find_food();
-					CELL* predator=scene1.cells[i]->spot_predators();
+					CELL* food=aevo.cells[i]->find_food();
+					CELL* predator=aevo.cells[i]->spot_predators();
 					if(predator!=NULL||food!=NULL)
 					{
-						vect a=(food->position()-scene1.cells[i]->position()).dir();
+						vect a=(food->position()-aevo.cells[i]->position()).dir();
 						if(predator)
 							a*=-20;
 						else if(food)
 							a*=20;
-						scene1.cells[i]->addacc(a);
+						aevo.cells[i]->addacc(a);
 					}
 				}
 
-					for(unsigned int j=0;j<scene1.cells.size();j++)
+					for(unsigned int j=0;j<aevo.cells.size();j++)
 					{
 						if(i!=j)
 						{
-							if(scene1.cells[i]->mash(*scene1.cells[j])&&scene1.cells[i]->continuous_contact()<2)
+							if(aevo.cells[i]->mash(*aevo.cells[j])&&aevo.cells[i]->continuous_contact()<2)
 								Mix_PlayChannel( -1, bounce_loud, 0 );
 						}
 					}
-					if(scene1.cells[i]->time_since_last_collision()>100)
+					if(aevo.cells[i]->time_since_last_collision()>100)
 					{
-						if(scene1.cells[i]->globalcollision(scene1.frametimer.deltatime()))
+						if(aevo.cells[i]->globalcollision(aevo.frametimer.deltatime()))
 						{
 							Mix_PlayChannel( -1, bounce, 0 );
 						}
 					}
-					scene1.cells[i]->integrate(scene1.frametimer.deltatime());
+					aevo.cells[i]->integrate(aevo.frametimer.deltatime());
 			}
 		}
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 		//.................................graphic rendering
-		scene1.DisplaySortCells();	//makes sure objects closer to the camera are displayed on top of objects further away
-		for(unsigned int i=0;i<scene1.cells.size();i++)	//display all objects on screen
+		aevo.DisplaySortCells();	//makes sure objects closer to the camera are displayed on top of objects further away
+		for(unsigned int i=0;i<aevo.cells.size();i++)	//display all objects on screen
 		{
-			scene1.cells[i]->display();
+			aevo.cells[i]->display();
 		}
 		//display various texts on screen
-		message.display(scene1);
-		fps_label.display(scene1);
-		fps.display(scene1);
-		particle_n_label.display(scene1);
-		particle_n.set(int(scene1.spheres.size()+scene1.cells.size()));	//store the number of particles currently on screen into the particle_n graphicstring object
-		particle_n.display(scene1);
-		bugs.display(scene1);
+		message.display(aevo);
+		fps_label.display(aevo);
+		fps.display(aevo);
+		particle_n_label.display(aevo);
+		particle_n.set(int(aevo.spheres.size()+aevo.cells.size()));	//store the number of particles currently on screen into the particle_n graphicstring object
+		particle_n.display(aevo);
+		bugs.display(aevo);
 		//.................................
 
 		//---------------------------------termination
-		fps.set(scene1.frametimer.currentfps());	//set the current fps into fps graphic string object
-		scene1.terminateframe(background);	//does necessary actions to terminate the current frame appropriately
-		if(scene1.frametimer.currentframe()>10000)	//ends the program if more than 10000 frames have been displayed
-			scene1.ended=true;
+		fps.set(aevo.frametimer.currentfps());	//set the current fps into fps graphic string object
+		aevo.terminateframe(background);	//does necessary actions to terminate the current frame appropriately
+		if(aevo.frametimer.currentframe()>10000)	//ends the program if more than 10000 frames have been displayed
+			aevo.ended=true;
 		//---------------------------------
 	}
 	//deallocate various dynamic allocations
