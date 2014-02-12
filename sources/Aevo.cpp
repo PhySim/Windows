a8996e25d5833bd6924e6a8a97b0a1eaf7ad7f31
Reuben John <reubezzz1@gmail.com>
2014-02-12 10:51:13 +0300

    reconfiguration (no more Debug/ + precaution: including unnecessary headers)

--- sources/Aevo.cpp
+++ sources/Aevo.cpp
@@ -1,5 +1,6 @@
 #include "headers/physim.h"
 #include <string>
+
 using namespace std;
 
 SDL_Surface* background;
--- sources/intergravity.cpp
+++ sources/intergravity.cpp
@@ -5,8 +5,8 @@ SDL_Event event;
 int main(int argc,char* args[])
 {
 	ofstream fout("framelog.txt");	//used to log frame rate statistics
-	PHYSIM scene1((vect){960,512,32});	//main variable containing the properties of the physical world
-	scene1.change_world_dimensions((vect){10000,10000,200000});
+	PHYSIM scene1((vect<>){960,512,32});	//main variable containing the properties of the physical world
+	scene1.change_world_dimensions((vect<>){10000,10000,200000});
 	scene1.camera_speed=100;
 	scene1.camera_pos.z=9000;
 	SDL_FillRect(scene1.scr,&scene1.scr->clip_rect,SDL_MapRGB(scene1.scr->format,0xDD,0xDD,0xDD));	//Initializes a grey background
@@ -32,11 +32,11 @@ int main(int argc,char* args[])
 		//=================================initialisation
 		if(scene1.frametimer.currentframe()==25)
 		{
-			scene1.gensphere(loadimage("images/Blackhole.png"),(vect){500,350,10000},(vect){100,100,100},pow((long double)10,18));
+			scene1.gensphere(loadimage("images/Blackhole.png"),(vect<>){500,350,10000},(vect<>){100,100,100},pow((long double)10,18));
 		}
 		if(scene1.frametimer.currentframe()%50==0)	//block that auto generates a new sphere every 50 frames and provides certain properties to it
 		{
-			scene1.gensphere(star,scene1.random_position(),(vect){20,20,20},pow((long double)10,10))->PARTICLE::addvel(random((vect){-1000,-1000,0},(vect){1000,1000,0}));
+			scene1.gensphere(star,scene1.random_position(),(vect<>){20,20,20},pow((long double)10,10))->PARTICLE::addvel(random((vect<>){-1000,-1000,0},(vect<>){1000,1000,0}));
 		}
 		scene1.initiateframe();
 		for(unsigned int i=1;i<scene1.spheres.size();i++)
@@ -53,13 +53,13 @@ int main(int argc,char* args[])
 			if( scene1.event.type == SDL_MOUSEBUTTONDOWN )	//check if the left mouse button has been pressed and then generates a new object at that position
 				if( scene1.event.button.button == SDL_BUTTON_LEFT )
 				{
-					vect newpos=scene1.real_position_of(scene1.mousepos,500);
+					vect<> newpos=scene1.real_position_of(scene1.mousepos,500);
 					if(newpos.z<0)
 						newpos.z=0;
 					else if(newpos.z>scene1.world_dim.z)
 						newpos.z=scene1.world_dim.z;
-					scene1.gensphere(copy_surface(ball),newpos,(vect){20,20,20},pow(10,15))->PARTICLE::addvel(random((vect){-50,-50,-50},(vect){50,50,50}));
-					//aevo.gensphere(loadimage(blue_ball),newpos,(vect){20,20,20});
+					scene1.gensphere(copy_surface(ball),newpos,(vect<>){20,20,20},pow(10,15))->PARTICLE::addvel(random((vect<>){-50,-50,-50},(vect<>){50,50,50}));
+					//aevo.gensphere(loadimage(blue_ball),newpos,(vect<>){20,20,20});
 				}
 		}
 		//_________________________________
